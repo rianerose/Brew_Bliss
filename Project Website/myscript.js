f7630a14a5e887f08befc8c75fcd3a3011d7b12f
rianerose <124267703+rianerose@users.noreply.github.com>
2024-11-06 18:56:42 +0800

    Add files via upload

---
+++ Project Website/myscript.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var hamburgerMenu = document.getElementById("hamburger-menu");
+    if (hamburgerMenu.style.display === "none") {
+        hamburgerMenu.style.display = "block";
+    } else {
+        hamburgerMenu.style.display = "none";
+}
+}
